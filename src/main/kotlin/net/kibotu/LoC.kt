7c9de8c318c5c2cef740aac2f68575a5f6224dc4
Jan Rabe <jan.rabe@exozet.com>
2018-09-15 13:38:33 +0200

    wip loc

---
+++ src/main/kotlin/net/kibotu/LoC.kt
@@ -0,0 +1,7 @@
+package net.kibotu
+
+// simulator https://github.com/WillNess210/LoCMSimulator
+// https://github.com/WillNess210/LoCMSimulator/blob/master/sources/main.cpp
+
+// player https://github.com/WillNess210/Legends-of-Code-and-Magic/blob/master/LCM/src/Player.java
+
